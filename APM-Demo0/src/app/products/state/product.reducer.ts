0413988e948456978ca7505ea99239f9ed032f9f
Almudena Blanco <ablanco@sorint.it>
2022-09-02 11:44:22 +0200

    feat: building selectors

--- APM-Demo0/src/app/products/state/product.reducer.ts
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -1,4 +1,5 @@
-import { createAction, on, createReducer } from "@ngrx/store";
+import { state } from "@angular/animations";
+import { createAction, on, createReducer, createFeatureSelector, createSelector } from "@ngrx/store";
 import * as AppState from '../../state/app.state'
 
 import { Product } from "../product";
@@ -17,6 +18,23 @@ const initialState : ProductState = {
     currentProduct: null,
     products: [],
 }
+
+const getProductFeatureState = createFeatureSelector<ProductState>('products');
+
+export const getShowProductCode = createSelector(
+    getProductFeatureState,
+    state => state.showProductCode
+)
+
+export const getCurrentProduct = createSelector(
+    getProductFeatureState,
+    state => state.currentProduct
+)
+
+export const getProducts = createSelector(
+    getProductFeatureState,
+    state => state.products
+)
 
 export const productReducer = createReducer<ProductState>(
     initialState,
