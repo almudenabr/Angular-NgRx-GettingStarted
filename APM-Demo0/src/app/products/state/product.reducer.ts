c019a379b7a991fae7ed258e5e4c094f3a3d988d
Almudena Blanco <ablanco@sorint.it>
2022-09-12 12:23:44 +0200

    feat: configure the reducer to listen the effect's action and clean up the service

--- APM-Demo0/src/app/products/state/product.reducer.ts
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -69,5 +69,11 @@ export const productReducer = createReducer<ProductState>(
 
             }
         }
+    }),
+    on(ProductActions.loadProductsSuccess, (state,action): ProductState => {
+        return {
+            ...state,
+            products: action.products
+        }
     })
 )
