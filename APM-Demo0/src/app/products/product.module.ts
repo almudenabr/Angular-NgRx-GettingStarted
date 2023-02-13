550a4a75b92d920d7e9290b4db187f04ab43a007
Almudena Blanco <ablanco@sorint.it>
2023-02-13 16:47:19 +0100

    refactor: move product.effects.ts to the correct folder (state)

--- APM-Demo0/src/app/products/product.module.ts
+++ APM-Demo0/src/app/products/product.module.ts
@@ -11,7 +11,7 @@ import { ProductEditComponent } from './product-edit/product-edit.component';
 import { StoreModule } from '@ngrx/store';
 import { productReducer } from './state/product.reducer';
 import { EffectsModule } from '@ngrx/effects';
-import { ProductEffects } from './products.effects';
+import { ProductEffects } from './state/product.effects';
 
 const productRoutes: Routes = [
   { path: '', component: ProductShellComponent }
--- APM-Demo0/src/app/products/state/product.effects.ts
+++ APM-Demo0/src/app/products/state/product.effects.ts
@@ -2,8 +2,8 @@ import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { of } from "rxjs";
 import { catchError, map, mergeMap } from "rxjs/operators";
-import { ProductService } from "./product.service";
-import * as ProductActions from "./state/product.actions";
+import { ProductService } from "../product.service";
+import * as ProductActions from "./product.actions";
 
 @Injectable()
 export class ProductEffects {
